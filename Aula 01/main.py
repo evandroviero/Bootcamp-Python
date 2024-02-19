2cf325a44adcee7c36df0fc6228a9749bb2f454b
evandroviero <evandro.viero@gmail.com>
2024-02-19 13:14:30 -0300

    aula 01

--- Aula 01/main.py
+++ Aula 01/main.py
@@ -1,1 +1,12 @@
 print('Hello World!')
+
+def calcular_bonus(nome, salario, bonus):
+    valor = 1000 + salario * bonus
+    return f'Olá {nome}, o seu valor final será {valor:.2f}'
+
+nome = input('Defina um nome: ')
+salario = float(input('Defina o valor: '))
+bonus = float(input('Digite um bonus: '))
+print(calcular_bonus(nome=nome, salario=salario, bonus=bonus))
+
+print('Final de execução')
