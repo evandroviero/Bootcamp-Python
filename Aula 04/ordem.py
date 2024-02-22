c181e6db6f898143590d43961dcad4c88ac3287e
evandroviero <evandro.viero@gmail.com>
2024-02-22 12:22:44 -0300

    Ajuste de erro

--- Aula 04/ordem.py
+++ Aula 04/ordem.py
@@ -1,13 +1,16 @@
 lista = [64, 34, 25, 12, 22, 11, 90]
 
 def ordernar_lista(lista: list) -> list:
-    lista_ordenada = lista.copy()
+    # lista_ordenada = lista.copy()
 
     for i in range(len(lista)):
         for j in range(i+1, len(lista)):
             if lista[i] > lista[j]:
                 lista[i], lista[j] = lista[j], lista[i]
 
-    return lista_ordenada
+    return lista
 
 print(lista)
+
+t = ordernar_lista(lista)
+print(t)
