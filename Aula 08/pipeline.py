4516d485f06755161a4237ae07a2682f2f387d5a
evandroviero <evandro.viero@gmail.com>
2024-02-28 13:02:12 -0300

    salvando tambem em parquet

--- Aula 08/pipeline.py
+++ Aula 08/pipeline.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     pasta_raiz = Path(__file__).parent
     pasta_entrada = pasta_raiz / 'data'
 
-    formato_saida = ["csv"]  # Ou 'parquet', conforme a decisão de saída
+    formato_saida = ["parquet","csv"]  # Ou 'parquet', conforme a decisão de saída
 
     pipeline(pasta_entrada, formato_saida)
